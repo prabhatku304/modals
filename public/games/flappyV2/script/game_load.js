bc6a3830c80f141374f5bcf4ecd59406fe7177c5
b117014 <prabhat@Prabhats-MacBook-Pro.local>
2024-02-02 13:21:50 +0530

    flappy v2

---
+++ public/games/flappyV2/script/game_load.js
@@ -0,0 +1,23 @@
+const onLoadGame = () => {
+  window.clickToPlay = "Click to play";
+  const gameContainer = document.createElement("div");
+  gameContainer.style.position = "relative";
+  gameContainer.innerHTML = `<div
+        id="currentScore"
+        class="text-center text-3xl absolute top-[10rem] left-0 right-0"
+        style="position: absolute; top:10rem; left:0; right:0; font-size: 24px; text-align: center"
+      ></div>
+      <canvas id="canvas" width="300" height="600"></canvas>`;
+
+  document.body.appendChild(gameContainer);
+};
+
+const appHeight = () => {
+  const doc = document.documentElement;
+  let vh = Math.min(window.innerHeight, doc.clientHeight) * 0.01;
+  doc.style.setProperty("--vh", `${vh}px`);
+};
+window.addEventListener("resize", appHeight);
+appHeight();
+
+onLoadGame();
